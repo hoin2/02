822f9d92e39eef399287e7fc0cff90e475932782
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-09-13 16:57:39 +0900

    mdification

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello, Sookmyung Electronics!\n");
+	int a, b;
+	a=10;
+	b=20;
+	printf("%d + %d = %d\n",a,b,a+b);
 	return 0;
 }
